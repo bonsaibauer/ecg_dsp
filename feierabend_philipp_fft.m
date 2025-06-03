f2972babe1128c8f0c9273bb79d0e8d57b6ecaaf
Philipp <mail@philippfeierabend.de>
2025-06-03 20:00:55 +0200

    update fft code

--- feierabend_philipp_fft.m
+++ feierabend_philipp_fft.m
@@ -1,11 +1,30 @@
-function Name_Vorname_fft(signal, t)
-    load('noisy_ecg.mat');
-    % FFT des gefilterten Signals
-    Y = fft(signal);
-    f = (0:length(Y)-1) * (1 / (t(2) - t(1))) / length(Y);
+function feierabend_philipp_fft(signal, t)
+    disp('-------------------------------------------------------------');
+    disp('Task 2.4');
+    disp('-------------------------------------------------------------');    
+
+    % Health Bot Introduction
+    disp('Health Bot: Now, let me perform the Fourier Transform on your ECG signal to analyze it in the frequency domain!'); 
+    pause(2);
+    
+    % Calculate the sampling frequency from the time vector
+    fs = 1 / (t(2) - t(1));  % Sampling frequency (in Hz)
+
+    % Perform the Fourier Transform on the signal to get frequency components
+    N = length(signal);  % Length of the signal
+    f = (0:N-1)*(fs/N);  % Frequency axis (in Hz)
+    signal_fft = abs(fft(signal));  % Magnitude of the Fourier Transform
+    
+    % Visualize the frequency spectrum
     figure;
-    plot(f, abs(Y));
-    xlabel('Frequenz [Hz]');
+    plot(f, signal_fft);  % Plot magnitude spectrum vs frequency
+    title('Magnitude Spectrum of ECG Signal');
+    xlabel('Frequenz (Hz)');
     ylabel('Magnitude');
-    title('Magnitudespektrum des gefilterten EKG-Signals');
+
+     % Assign the filtered signal to the global workspace as y_f
+     assignin('base', 'y_fft', signal_fft);  % 'base' specifies the global workspace
+    
+    % Health Bot confirmation message for successful FFT analysis
+    disp('Health Bot: Here is the frequency spectrum of your ECG signal!');
 end
